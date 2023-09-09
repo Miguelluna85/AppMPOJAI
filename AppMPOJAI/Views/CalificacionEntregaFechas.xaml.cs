aaf5b334f1276132f09d069c076456f7eb0d8dfa
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2023-09-09 13:13:48 -0600

    Entregas por granja part1

---
+++ AppMPOJAI/Views/CalificacionEntregaFechas.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMPOJAI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalificacionEntregaFechas : ContentPage
+    {
+        public CalificacionEntregaFechas()
+        {
+            InitializeComponent();
+        }
+    }
+}
